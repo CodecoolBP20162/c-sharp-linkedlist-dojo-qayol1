ef347500d08b9a67f863b8e00de080ebc5633b60
qayol1 <qayol@freemail.hu>
2017-06-29 11:39:36 +0200

    started

---
+++ ListImplementation/ListImplementation/SinglyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImplementation
+{
+    public class SinglyList<T>
+    {
+        
+        private class Node
+        {
+            public Node Next;
+            public T Data;
+        }
+
+        private Node head;
+
+        public void Add(T data)
+        {
+            Node toAdd = new Node();
+            toAdd.Data = data;
+            if (head != null)
+            {
+                head = toAdd;
+            } else
+            {
+                Node current = head;
+                current.Next = toAdd;
+            }
+            
+        }
+    }
+}
---
+++ ListImplementation/ListImplementationTests/SinglyListTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImplementation.Tests
+{
+    [TestClass()]
+    public class SinglyListTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            SinglyList<String> testList = new SinglyList<string>();
+            testList.Add("aaaa");
+        }
+    }
+}
